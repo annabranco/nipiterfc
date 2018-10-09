c2e29bdf894dfcee3a132b39ff88c88ac9141757
Anna <annya.branco@gmail.com>
2018-10-10 01:46:50 +0200

    add: GH Pages v0.1.0

---
+++ docs/precache-manifest.a53457d930d87a2b4e8ca34c8dd6186d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dfa96b5a1701f77f00938f9e8d34405",
+    "url": "./static/media/escudo_intro.7dfa96b5.gif"
+  },
+  {
+    "revision": "409ea77e6ca6fc43e989",
+    "url": "./static/css/main.3493725c.chunk.css"
+  },
+  {
+    "revision": "ea586d5355effe789fc7",
+    "url": "./static/js/1.ea586d53.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "67c263fe7803778665ebd87d88ba2031",
+    "url": "./static/media/nipiter_bar_top.67c263fe.jpg"
+  },
+  {
+    "revision": "6c7ffdcd853842fb0bb14fef1ece36c2",
+    "url": "./static/media/escudo.6c7ffdcd.png"
+  },
+  {
+    "revision": "409ea77e6ca6fc43e989",
+    "url": "./static/js/main.409ea77e.chunk.js"
+  },
+  {
+    "revision": "5440081399179e3bd0cd3b92a8b6b776",
+    "url": "./static/media/1-diana.54400813.jpg"
+  },
+  {
+    "revision": "5ec8a49df80e93f103bd71daaa912956",
+    "url": "./static/media/4-lara.5ec8a49d.jpg"
+  },
+  {
+    "revision": "32bf38bf94b38932d4807c1241c88af9",
+    "url": "./static/media/9-merida.32bf38bf.jpg"
+  },
+  {
+    "revision": "3b9b251ca085d2e38f3cdd68e27059e6",
+    "url": "./static/media/13-lisa.3b9b251c.jpg"
+  },
+  {
+    "revision": "d4713996af8c9a7dabb9b2e5ef779f23",
+    "url": "./static/media/nipiter_bar_bottom.d4713996.jpg"
+  },
+  {
+    "revision": "c90af4c364d4cc10b553b607e3ce8def",
+    "url": "./index.html"
+  }
+];
