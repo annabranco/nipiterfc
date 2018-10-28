9e7be5da80d6cc33259fa4e9ccf6aff3c143ca88
Anna <annya.branco@gmail.com>
2018-10-28 18:05:44 +0100

    upd: version number

--- src/core/App.js
+++ src/core/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import NiPiter from '../components/NiPiter';
 
 class App extends Component {
+  constructor ( props ) {
+    super( props );
+
+    this.state = { version: 'v0.2.0' };
+  }
 
   render () {
+    const { version } = this.state;
+
     return (
       <div className="App">
-        <NiPiter />
+        <NiPiter version={ version } />
       </div>
     );
   }
 }
 
 export default App;
+
+App.propTypes = { version: PropTypes.string };
